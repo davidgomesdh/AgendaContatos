641fddcd14aca8a4ba6a9b9a219442a974ed754c
David Gomes dos Santos <david.santos@zapcorp.com.br>
2018-03-03 09:46:08 -0300

    Em construção

--- app/src/main/java/com/example/davidsantos/agendacontatos/ContatoInfo.java
+++ app/src/main/java/com/example/davidsantos/agendacontatos/ContatoInfo.java
@@ -1,10 +1,13 @@
 package com.example.davidsantos.agendacontatos;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by david.santos on 02/03/2018.
  */
 
-public class ContatoInfo {
+public class ContatoInfo implements Parcelable {
 
     private String nome = "";
     private String ref = "";
@@ -12,6 +15,22 @@ public class ContatoInfo {
     private String fone = "";
     private String end = "";
     private String foto = "";
+
+    ContatoInfo(){
+
+    }
+
+    private ContatoInfo(Parcel in){
+        String[] data = new String[6];
+        in.readStringArray(data);
+
+        setNome(data[0]);
+        setRef(data[1]);
+        setEmail(data[2]);
+        setFone(data[3]);
+        setEnd(data[4]);
+        setFoto(data[5]);
+    }
 
     public String getNome() {
         return nome;
@@ -60,4 +79,30 @@ public class ContatoInfo {
     public void setFoto(String foto) {
         this.foto = foto;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringArray(new String[]{
+                getNome(),getRef(),getEmail(),getFone(), getEnd(),getFoto()
+        });
+    }
+
+    public static final Parcelable.Creator<ContatoInfo> CREATOR = new Parcelable.Creator<ContatoInfo>(){
+
+        @Override
+        public ContatoInfo createFromParcel(Parcel source) {
+            return new ContatoInfo(source);
+        }
+
+        @Override
+        public ContatoInfo[] newArray(int size) {
+            return new ContatoInfo[size];
+        }
+    };
+
 }
--- app/src/main/java/com/example/davidsantos/agendacontatos/EditActivity.java
+++ app/src/main/java/com/example/davidsantos/agendacontatos/EditActivity.java
@@ -2,12 +2,39 @@ package com.example.davidsantos.agendacontatos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class EditActivity extends AppCompatActivity {
+
+    private ContatoInfo contato;
+
+    private ImageButton foto;
+    private EditText nome;
+    private EditText ref;
+    private EditText email;
+    private EditText fone;
+    private EditText endereco;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        contato = getIntent().getParcelableExtra("contato");
+
+        foto = findViewById(R.id.fotoContato);
+        nome = findViewById(R.id.nomeContato);
+        ref = findViewById(R.id.refContato);
+        email = findViewById(R.id.emailContato);
+        fone = findViewById(R.id.foneContato);
+        endereco = findViewById(R.id.endContato);
+
+        nome.setText(contato.getNome());
+        ref.setText(contato.getRef());
+        email.setText(contato.getEmail());
+        fone.setText(contato.getFone());
+        endereco.setText(contato.getEnd());
     }
 }
